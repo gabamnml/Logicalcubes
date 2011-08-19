768e6ffaf01805b14f9d343c6c2495a4801a6bfe
Gabriel Aramburu <aramburu.gabriel@gmail.com>
2011-08-19 19:30:41 -0300

    Loading data to use orchestra

--- conex.php
+++ conex.php
@@ -1,12 +1,12 @@
 <?php
 function Conectarse()
 {
-   if (!($link=mysql_connect("localhost","DB_USER","DB_PASSWORD")))
+   if (!($link=mysql_connect("10.194.111.8","user_8827137c","3ERaeAnYP_$,y3")))
    {
       echo "Error conectando a la base de datos.";
       exit();
    }
-   if (!mysql_select_db("DB_NAME",$link))
+   if (!mysql_select_db("db_8827137c",$link))
    {
       echo "Error seleccionando la base de datos.";
       exit();
